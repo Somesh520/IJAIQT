a2cf99d61b219895077f8cc3dfca017a9f64b0cc
somesh520 <someshtiwari.in@gmail.com>
2026-09-18 10:09:22 +0530

    empty pages wirte the progeess report

--- frontend/app/faq/page.tsx
+++ frontend/app/faq/page.tsx
@@ -29,7 +29,7 @@ export default function FAQPage() {
 
   return (
     <main className="w-full bg-white min-h-[600px] px-2 py-4 md:px-6">
-      <div className="max-w-5xl mx-auto bg-white p-6 text-[24px] leading-relaxed text-gray-900">
+      <div className="max-w-5xl mx-auto bg-white p-6 text-[26px] leading-relaxed text-gray-900">
         <div
           className="prose max-w-none prose-slate text-justify [&_p]:!text-[18px] [&_span]:!text-[17px] [&_strong]:!text-[17px] [&_b]:!text-[17px] [&_li]:!text-[17px] [&_h3]:!text-[17px] [&_h4]:!text-[17px] [&_p]:!mb-1 [&_p]:!mt-0 [&_p:empty]:hidden [&_h3]:!mt-2 [&_h3]:!mb-0 [&_h4]:!mt-2 [&_h4]:!mb-0 [&_h1]:!text-[36px] [&_h1]:!font-bold [&_h1]:!mb-6 [&_h1]:!mt-4 [&_h2]:!text-[30px] [&_h2]:!font-bold [&_h2]:!mb-4 [&_h2]:!mt-8"
           dangerouslySetInnerHTML={{ __html: htmlContent }}
