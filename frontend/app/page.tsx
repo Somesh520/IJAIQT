fd8eaf5a177dcecdf510d554ccd41977019057d2
somesh520 <someshtiwari.in@gmail.com>
2026-09-18 10:00:40 +0530

    removing coloumn in important links

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -211,7 +211,7 @@ export default function HomePage() {
             {/* Important Links */}
             <div className="w-full" style={{ backgroundColor: themeColor }}>
               <div className="px-3 py-2 border-b border-white/20 text-center">
-                <span className="font-bold  text-[16px] leading-tight " style={{ color: orangeColor }}>Important Links:</span>
+                <span className="font-bold  text-[16px] leading-tight " style={{ color: orangeColor }}>Important Links</span>
               </div>
               {[
                 { label: 'Instructions to Authors', href: '/authors' },
